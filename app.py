59f1fdb0b635d269d5e0305256538e6f09cef84b
Thiago Alves <107894642+Thiagoalves033@users.noreply.github.com>
2023-09-14 23:17:13 -0300

    Login and Register routes added and minor fixes

--- app.py
+++ app.py
@@ -6,3 +6,13 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")

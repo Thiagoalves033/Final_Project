3521c3922132168992774832a4e6ee7772531c6f
Thiago Alves <107894642+Thiagoalves033@users.noreply.github.com>
2023-09-23 17:13:48 -0300

    Added more tables into the database

--- htools.sql
+++ htools.sql
@@ -3,3 +3,27 @@ CREATE TABLE users (
     username TEXT NOT NULL,
     hash TEXT NOT NULL
 );
+
+CREATE TABLE profiles (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+CREATE TABLE appointments (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    profile_id INTEGER NOT NULL,
+    specialty TEXT NOT NULL,
+    date DATE,
+    time TIME,
+    FOREIGN KEY (profile_id) REFERENCES profiles (id)
+);
+
+CREATE TABLE vaccination (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    profile_id INTEGER NOT NULL,
+    vaccine TEXT NOT NULL,
+    dose TEXT NOT NULL,
+    date DATE,
+    FOREIGN KEY (profile_id) REFERENCES profiles (id)
+);

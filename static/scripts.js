9b6f4bdbd41cf4b745c728e55bad50b370e22cdf
Thiago Alves <107894642+Thiagoalves033@users.noreply.github.com>
2023-09-26 23:48:52 -0300

    Suffering with the scripts for hours

--- static/scripts.js
+++ static/scripts.js
@@ -1,49 +1,13 @@
-// Appointments
-
-document.addEventListener('DOMContentLoaded', function() {
-    let btnAdd = document.querySelector('#ap_button');
-    let table = document.querySelector('#ap_table');
-    let specialtyInput = document.querySelector('#specialty');
-    let dateInput = document.querySelector('#ap_date');
-    let hourInput = document.querySelector('#hour');
-
-    btnAdd.addEventListener('click', () => {
-        let specialty = specialtyInput.value;
-        let date = dateInput.value;
-        let hour = hourInput.value;
-
-        let template = `
-                    <tr>
-                        <td>${specialty}</td>
-                        <td>${date}</td>
-                        <td>${hour}</td>
-                    </tr>`;
-
-    table.innerHTML += template;
+$(document).ready(function() {
+    $('.nav-link').on('click', function() {
+        var activeTabId = $(this).attr('id');
+        
+        $('#activeTab').val(activeTabId);
+        
+        $(document).trigger('tabClicked', activeTabId);
     });
-});
-
-// Vaccination
-
-document.addEventListener('DOMContentLoaded', function() {
-    let btnAdd = document.querySelector('#va_button');
-    let table = document.querySelector('#va_table');
-    let vaccineInput = document.querySelector('#vaccine');
-    let doseInput = document.querySelector('#dose');
-    let dateInput = document.querySelector('#va_date');
-
-    btnAdd.addEventListener('click', () => {
-        let vaccine = vaccineInput.value;
-        let dose = doseInput.value;
-        let date = dateInput.value;
-
-        let template = `
-                    <tr>
-                        <td>${vaccine}</td>
-                        <td>${dose}</td>
-                        <td>${date}</td>
-                    </tr>`;
-
-    table.innerHTML += template;
+    
+    $(document).on('tabClicked', function(event, activeTabId) {
+        console.log(activeTabId);
     });
 });

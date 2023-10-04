70c8748514436c0d7127c88d84c0225f2f167afc
Thiago Alves <107894642+Thiagoalves033@users.noreply.github.com>
2023-10-03 22:35:32 -0300

    Removed vaccine and appointments tables

--- htools.sql
+++ htools.sql
@@ -18,21 +18,3 @@ CREATE TABLE profiles (
     alcohol TEXT NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users (id)
 );
-
-CREATE TABLE appointments (
-    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    profile_id INTEGER NOT NULL,
-    specialty TEXT NOT NULL,
-    date DATE,
-    time TIME,
-    FOREIGN KEY (profile_id) REFERENCES profiles (id)
-);
-
-CREATE TABLE vaccination (
-    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    profile_id INTEGER NOT NULL,
-    vaccine TEXT NOT NULL,
-    dose TEXT NOT NULL,
-    date DATE,
-    FOREIGN KEY (profile_id) REFERENCES profiles (id)
-);
